63d9ca4a87c11975484c242c5ae160cbeab95c00
HOAITGL <minhhhoa.gl1982@gmail.com>
2025-07-10 22:20:26 +0700

    Fix lỗi SERIAL không tương thích với SQLite

--- seed.py
+++ seed.py
@@ -7,7 +7,7 @@ from flask import Flask
 from models.leave_request import LeaveRequest
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database.db')  # fallback nếu thiếu env
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") or 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
